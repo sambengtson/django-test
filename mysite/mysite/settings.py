7b7d706ad07209891eb20addd0a0c2b9701e62c6
Sam <samb@tkc32m.com>
2023-05-24 11:25:20 -0500

    more cors

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello_world',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -145,4 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    '*',
+]
